4d2c9b6782692accab058c744f883dcb64d8fddf
Noah A <nbaltunian@gmail.com>
2015-03-09 22:11:31 +0000

    Added assertions to park_test.rb

--- test/models/park_test.rb
+++ test/models/park_test.rb
@@ -1,7 +1,10 @@
 require 'test_helper'
 
 class ParkTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+#   test "the truth" do
+#     assert true
+#   end
+  test "has failed" do
+    flunk ( ["This test is not finished."] )
+  end
 end
