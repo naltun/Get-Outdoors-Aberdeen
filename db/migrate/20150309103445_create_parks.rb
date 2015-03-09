407e8c4b483f2b8eb69d71f64f979858dee6885e
Noah A <nbaltunian@gmail.com>
2015-03-09 21:12:20 +0000

    Configured db and migrated

--- db/migrate/20150309103445_create_parks.rb
+++ db/migrate/20150309103445_create_parks.rb
@@ -1,6 +1,12 @@
 class CreateParks < ActiveRecord::Migration
   def change
     create_table :parks do |t|
+      t.text :name
+      t.text :openTime
+      t.text :closeTime
+      t.text :adress
+      t.text :busRoutes
+      t.text :description
 
       t.timestamps null: false
     end
