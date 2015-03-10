4fefffea61273ee423dfa32d1db97d349f41c09a
Noah A <nbaltunian@gmail.com>
2015-03-10 01:12:21 +0000

    Fixing nav

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,10 +3,7 @@ class ApplicationController < ActionController::Base
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
   
-#   def index
-#     render :index
-#   end
-#   def parks
-#     render :parks
-#   end
+  def parks
+    render :parks
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   root 'application#index'
+  get '/parks', to: 'application#parks'
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
