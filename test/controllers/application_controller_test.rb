35a4e56a5d263c4f05fc323c2a38fc42c98ee5cc
Noah A <nbaltunian@gmail.com>
2015-03-13 01:06:56 +0000

    Updated application controller tests

--- test/controllers/application_controller_test.rb
+++ test/controllers/application_controller_test.rb
@@ -1,7 +1,20 @@
 require 'test_helper'
 
 class ApplicationControllerTest < ActionController::TestCase
-  test "fails" do
-    flunk( ["This test is not finished"] )
-  end    
+
+  test "should get home" do
+    get :home
+    assert_template 'home', "index was not rendered"
+  end
+  test "should get parks" do
+    get :parks
+    assert_template 'parks', "parks was not rendered"
+  end
+  test "should get map" do
+    get :map
+    assert_template 'map', "map was not rendered"
+  end
+#   test "fails" do
+#     flunk( ["This test is not finished"] )
+#   end    
 end
