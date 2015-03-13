cc35bdaf9714af16f079436ba2431f4a0a859ea8
Noah A <nbaltunian@gmail.com>
2015-03-13 17:59:30 +0000

    Seeded db

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,41 +1,54 @@
 Park.delete_all
 Park.create(
   name: "Allan Park",
-  openTime: "openTime",
-  closeTime: "closeTime",
-  address: "address",
-  busRoutes: "busRoutes",
-  description: "To answer the concern in the accepted answer If you import a directory, how can you determine import order? There's no way that doesn't introduce some new level of complexity.
-
-Some would argue that organizing your files into directories can REDUCE complexity.
-
-My organization's project is a rather complex app. There are 119 Sass files in 17 directories. These correspond roughly to our views and are mainly used for adjustments, with the heavy lifting being handled by our custom framework. To me, a few lines of imported directories is a tad less complex than 119 lines of imported filenames.",
-  image_url: "http://www.aberdeencity.gov.uk/web/MultimediaFiles/PARKS_SEATON.JPG",
-  image_attribution: "image_attribution"
+  openTime: "8am",
+  closeTime: "1hr before dusk",
+  address: "Allan Park, Park Brae, Cults",
+  busRoutes: "Stagecoach: 201, 202, 203- Royal Deeside",
+  description: "A park of 6 hectares, almost hidden from view in a sheltered hollow between the Deeside Walkway and the River Dee at Cults. The park has woods, bog garden and cricket pitch, with the main feature being the bog garden with its wide variety of marsh and water plants. This park was gifted to the community to mark Queen Victoria's long reign.",
+  image_url: "N/A",
+  image_attribution: "N/A"
 )
 
 Park.create(
-  name: "The Park",
-  openTime: "openTime",
-  closeTime: "closeTime",
-  address: "address",
-  busRoutes: "busRoutes",
-  description: "To answer the concern in the accepted answer If you import a directory, how can you determine import order? There's no way that doesn't introduce some new level of complexity.
-
-Some would argue that organizing your files into directories can REDUCE complexity.
-
-My organization's project is a rather complex app. There are 119 Sass files in 17 directories. These correspond roughly to our views and are mainly used for adjustments, with the heavy lifting being handled by our custom framework. To me, a few lines of imported directories is a tad less complex than 119 lines of imported filenames.",
-  image_url: "http://www.aberdeencity.gov.uk/web/MultimediaFiles/PARKS_JOHNSTON.JPG",
-  image_attribution: "image_attribution"
+  name: "Beach/Queens Links",
+  openTime: "This area is not enclosed",
+  closeTime: "This area is not enclosed",
+  address: "Queens Links, Beach Boulevard/Beach Esplanade, Aberdeen",
+  busRoutes: "N/A",
+  description: "A park of 6 hectares, almost hidden from view in a sheltered hollow between the Deeside Walkway and the River Dee at Cults. The park has woods, bog garden and cricket pitch, with the main feature being the bog garden with its wide variety of marsh and water plants. This park was gifted to the community to mark Queen Victoria's long reign. Aberdeen Beach is a Resort Seaside Award beach. The award, which is granted by Keep Scotland Beautiful, recognises excellent litter management, safety procedures and water quality. Aberdeen Beach has received the award every year since 2007.",
+  image_url: "http://www.aberdeencity.gov.uk/web/MultimediaFiles/BALLROOM_BEACH.JPG",
+  image_attribution: "aberdeencity.gov.uk"
 )
 
 Park.create(
-  name: "name",
-  openTime: "openTime",
-  closeTime: "closeTime",
-  address: "address",
-  busRoutes: "busRoutes",
-  description: "description",
-  image_url: "image_url",
-  image_attribution: "image_attribution"
+  name: "Bon Accord Terrace Gardens",
+  openTime: "This area is not enclosed",
+  closeTime: "This area is not enclosed",
+  address: "Bon Accord Gardens, Willowbank Road, Aberdeen",
+  busRoutes: "Stagecoach: X6, X7, X8, 204",
+  description: "A park of two hectares, is believed to be the site of a bloody battle over 300 years ago – ‘The Battle of Justice Mills’. The land which forms the current park was once the gardens of the houses on Bon Accord Crescent, and when these were turned into offices the land was first used as allotments gardens and then transformed into a park in the mid 1970s. The park provides a green respite close to the bustle of Union Street.",
+  image_url: "N/A",
+  image_attribution: "aberdeencity.gov.uk"
+)
+
+Park.create(
+  name: "Duthie Park",
+  openTime: "8am",
+  closeTime: "1hr before dusk",
+  address: "Duthie Park, Polmuir Road, Aberdeen",
+  busRoutes: "Stagecoach: X6, X7, X8, 204",
+  description: "Duthie Park is a park of 44 acres which was donated to the city by Miss Duthie in 1880 and opened to the public in 1883. The park has many artefacts including a bandstand, fountains, ponds, and statues. Within the park is the Winter Gardens, which were rebuilt in 1970. These house many exotic plants including the largest collection of cacti in Britain. This is a park for all the family, with activities from boating in the ponds to cricket on the lawns. The park has cricket on the lawns, bands in the park in summer, and the ponds are to be refurbished if the City receives heritage lottery funding. There are two children's play areas, a cafe with toilets and toilets within the Winter Gardens. Duthie Park is also home to the David Welch Winter Gardens.",
+  image_url: "http://www.aberdeencity.gov.uk/web/MultimediaFiles/WINTER_GARDENS.JPG",
+  image_attribution: "aberdeencity.gov.uk"
+)
+Park.create(
+  name: "Hazlehead Park",
+  openTime: "8am",
+  closeTime: "1hr before dusk",
+  address: "Hazlehead Park, Hazlehead Avenue, Aberdeen",
+  busRoutes: "Stagecoach: X17, 200, 210",
+  description: "A park of over 180 hectares, which was originally part of the freedom lands granted to the city in 1319. The land fell into private ownership but was bought back by the council in 1920.  This park is on the outskirts of Aberdeen and is heavily wooded. It is popular with sports enthusiasts, walkers, naturalists and picnickers. Around the park are football pitches, a golf course, pitch and putt course and woods for walking. The park itself has rose gardens, azalea & rhododendron borders, heather beds, a children's play area and a pets’ corner. The park has a significant collection of sculpture by a range of artists and heritage items which have been rescued from various places within the city.",
+  image_url: "http://www.aberdeencity.gov.uk/web/multimediafiles/hazlehead_park.jpg",
+  image_attribution: "aberdeencity.gov.uk"
 )
