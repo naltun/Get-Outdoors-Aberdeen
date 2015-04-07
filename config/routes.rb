ca827c4e9cd04aa3c7a749452fcba91c34c4571a
Noah A <nbaltunian@gmail.com>
2015-04-07 19:58:04 +0000

    Deleted old  material, added signup

--- config/routes.rb
+++ config/routes.rb
@@ -4,16 +4,21 @@ Rails.application.routes.draw do
   root 'application#index'
   get '/parks', to: 'application#parks'
   get '/map', to: 'application#map'
+  Rails.application.routes.draw do
+    get 'signup'  => 'users#new'
+    resources :users
+  end
+  
 #   get '/login', to: 'application#login'
   
-  Rails.application.routes.draw do
-  get 'users/new'
+#   Rails.application.routes.draw do
+#   get 'users/new'
 
-    resources :admins
-  end
+#     resources :admins
+#   end
 
-  get "/log-in" => "sessions#new", as: :log_in
-  post "/log-in" => "sessions#create"
-  get "/log-out" => "sessions#destroy", as: :log_out
+#   get "/log-in" => "sessions#new", as: :log_in
+#   post "/log-in" => "sessions#create"
+#   get "/log-out" => "sessions#destroy", as: :log_out
   
 end
