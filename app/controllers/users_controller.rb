54131b551f1be3332d1c119d16bac49463ed1aa2
Noah A <nbaltunian@gmail.com>
2015-04-09 13:05:32 +0000

    Finished login/logout and tests

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -11,6 +11,7 @@ class UsersController < ApplicationController
   def create
     @user = User.new(user_params)
     if @user.save
+      log_in @user
       flash[:success] = "Welcome user, now find your park and get outdoors!"
       redirect_to @user
     else
